b1997a03a146072c5202b8424e0081a0dd54616c
KatIsCoding <fabrigoru@gmail.com>
2021-11-30 17:24:54 -0600

    Add second excerice's queries

--- queries.sql
+++ queries.sql
@@ -7,3 +7,12 @@ SELECT date_of_birth from animals where name = 'Agumon' or name = 'Pickachu';
 SELECT name, escape_attempts from animals where weight_kg > 10.5;
 SELECT * from animals where neutered = true;
 SELECT * from animals where weight_kg between 10.4 and 17.3;
+
+/* Second Excercise Queries */
+
+SELECT count(*) from animals;
+SELECT count(*) from animals where escape_attempts = 0;
+SELECT avg(weight_kg) from animals;
+SELECT neutered, avg(escape_attempts) as escapes_average from animals group by neutered;
+SELECT species as type, max(weight_kg) as max_weight, min(weight_kg) as min_weight from animals group by species;
+SELECT species as type, avg(escape_attempts) as escapes_average from animals where date_of_birth BETWEEN '1990-01-01' AND '2000-01-01' group by type;
