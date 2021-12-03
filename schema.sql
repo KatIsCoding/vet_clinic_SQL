aeb6987aad5c06aa8f079167aae9af513c25e1f6
KatIsCoding <fabrigoru@gmail.com>
2021-12-03 14:03:03 -0600

    Refactor animals schema into relational data bases

--- schema.sql
+++ schema.sql
@@ -3,15 +3,31 @@
 /* psql vet_clinic */
 
 CREATE TABLE animals (
-    id integer NOT NULL,
+    id SERIAL,
     name text NOT NULL,
     date_of_birth date NOT NULL,
     escape_attempts integer NOT NULL,
     neutered boolean NOT NULL,
     weight_kg decimal NOT NULL,
+    species_id integer,
+    owner_id integer,
     PRIMARY KEY (id)
 );
 
-/* Second Excercise Queries */
+CREATE TABLE owners (
+    id SERIAL,
+    full_name text NOT NULL,
+    age integer NOT NULL,
+    PRIMARY KEY (id)
+);
 
-ALTER TABLE animals ADD species text;
+CREATE TABLE species (
+    id SERIAL,
+    name text NOT NULL,
+    PRIMARY KEY (id)
+);
+
+ALTER table animals ADD CONSTRAINT r_species foreign key (species_id) references species (id);
+ALTER table animals ADD CONSTRAINT r_owners foreign key (owner_id) references owners (id);
+
+
